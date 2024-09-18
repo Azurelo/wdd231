fee20dd3fc80cb3c99da48a4450957bc9cc7061e
Justus <galaxycheesemc@gmail.com>
2024-09-18 12:15:17 -0700

    content and structure homepage

---
+++ scripts/index.js
@@ -0,0 +1,27 @@
+document.getElementById('all-btn').addEventListener('click', () => {
+    showAllCourses();
+});
+
+document.getElementById('cse-btn').addEventListener('click', () => {
+    filterCourses('cse');
+});
+
+document.getElementById('wdd-btn').addEventListener('click', () => {
+    filterCourses('wdd');
+});
+
+function showAllCourses() {
+    const courses = document.querySelectorAll('.certificate-section button');
+    courses.forEach(course => course.style.display = 'block');
+}
+
+function filterCourses(type) {
+    const courses = document.querySelectorAll('.certificate-section button');
+    courses.forEach(course => {
+        if (!course.classList.contains(type)) {
+            course.style.display = 'none';
+        } else {
+            course.style.display = 'block';
+        }
+    });
+}
